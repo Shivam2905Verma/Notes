8a11ef7d9c9d792ccfba39f6f7346a9020ddf109
Shivam2905Verma <shivam7355g@gmail.com>
2026-09-24 01:30:05 +0530

    Level four is completed

--- test.js
+++ test.js
@@ -1,24 +1,25 @@
-// Create a buffer of 2 bytes
-const buffer = new ArrayBuffer(2);
+const stream = new ReadableStream({
+  start(controller) {
+    controller.enqueue("Hello");
+    controller.enqueue("Boss");
+    controller.enqueue("How");
+    controller.enqueue("are");
+    controller.enqueue("you?");
 
-// View it as 8-bit integers (shows the split bytes)
-const view8 = new Uint8Array(buffer);
-view8[0] = 244;
-view8[1] = 1;
+    controller.close();
+  },
+});
 
-const view = new DataView(buffer);
+const reader = stream.getReader();
 
-console.log(view.getUint16(0, true)); // Start reading from byte offset 0.
+while (true) {
+  const { value, done } = await reader.read();
 
-/**
- * to store value using dataview 
- * setUint16(
-    ① byteOffset,
-    ② value,
-    ③ littleEndian
-)
- */
+  if (done) {
+    break;
+  }
 
-// View the exact same buffer as a 16-bit integer
-const view16 = new Uint16Array(buffer);
-console.log(view16[0]);
+  console.log(value);
+}
+
+structuredClone;
