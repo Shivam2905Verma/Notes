35421eef11e33fc4ad78600834a67813f4e7763b
Shivam2905Verma <shivam7355g@gmail.com>
2026-09-23 16:40:40 +0530

    add ArrayBuffer ,DataView and some extra information at the level four

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
-const buffer = new ArrayBuffer(8);
+// Create a buffer of 2 bytes
+const buffer = new ArrayBuffer(2);
 
-console.log(buffer.byteLength);
+// View it as 8-bit integers (shows the split bytes)
+const view8 = new Uint8Array(buffer);
+view8[0] = 244;
+view8[1] = 1;
 
-const bytes = new Uint8Array(buffer);
+const view = new DataView(buffer);
 
-bytes[0] = "shivam";
-bytes[1] = 200;
+console.log(view.getUint16(0, true)); // Start reading from byte offset 0.
 
-console.log(bytes);
-console.log(buffer);
+/**
+ * to store value using dataview 
+ * setUint16(
+    ① byteOffset,
+    ② value,
+    ③ littleEndian
+)
+ */
+
+// View the exact same buffer as a 16-bit integer
+const view16 = new Uint16Array(buffer);
+console.log(view16[0]);
